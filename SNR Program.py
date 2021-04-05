5363e9763c05b653985e7e048c99308c2868621c
Abdullah Assaf <81966682+a-assaf@users.noreply.github.com>
2021-04-05 07:32:50 -0500

    SNR

---
+++ SNR Program.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+import math
+import matplotlib.pyplot as plt
+
+photons = float(input("What is total number of photons (signal)? "))
+pixels = float(input("What is the number of pixels? "))
+sky = float(input("What is the total number of photons per pixel from the background or sky? "))
+darke = 0
+#darke = float(input("What is the total number of dark current electrons per pixel? "))
+#reade = float(input("What is the total number of electrons per pixel resulting from the read noise? "))
+reade = 8.2   
+#equation for the S/N of a measurement made with a CCD
+
+ratio = photons / math.sqrt(photons + pixels*(sky + darke + (reade**2)))
+print('The SNR is: ', ratio)
+
+
+# In[ ]:
+
+
+
+
